f30aefce68dc1652eab83a006fbf0139983b8b6b
CAMILO <cestrad5@gmail.com>
2023-01-04 10:59:34 -0500

    Testing the new functions

--- main.c
+++ main.c
@@ -1,93 +1,24 @@
 #include "monty.h"
 
+globals_t globals;
+
 /**
- * check_is_number - check if a number is a number
+ * main - entry point to monty program.
+ * @argc: numbers of arguments.
+ * @argv: pointer to an array of string.
  *
- * @str: is the string to check
- * @line_number: number of the line
- * Return: Always 0
+ * Return: EXIT_SUCCESS.
  */
-void check_is_number(char *str, unsigned int line_number)
+int main(int argc, char *argv[])
 {
-    int i = 0;
+    stack_t *stack;
 
-    while (str[i] != '\0')
-    {
-        if (str[i] < '0' || str[i] > '9')
-        {
-            fprintf(stderr, "L%d: usage: push integer\n", line_number);
-            exit(EXIT_FAILURE);
-        }
-        i++;
-    }
-}
+    stack = NULL;
+    parse_arg(argc, argv);
+    read_line(&stack);
 
-/**
- * parse_method - function to parse the stack
- * @content: all the content of the stack
- * @line_number: the line number
- * Return: Always 0
- */
-void parse_method(char *content, unsigned int line_number)
-{
-    char *args;
-
-    void (*method_ptr)(stack_t * *stack, unsigned int line_number);
-    char *method = strtok(content, " $\n");
-
-    if (!method || method[0] == ' ')
-        return;
-    args = strtok(NULL, " $\n");
-
-    if (args && (strcmp(method, "push") == 0))
-    {
-        check_is_number(args, line_number);
-        global_stack = strdup(args);
-    }
-
-    method_ptr = get_method(method);
-    if (method_ptr == NULL)
-    {
-        fprintf(stderr, "L%d: unknown instruction %s\n", line_number, method);
-        exit(EXIT_FAILURE);
-    }
-
-    method_ptr(&global_stack_head, line_number);
-}
-/**
- * main - Implement main function
- *@argc: argument
- *@argv: argument 2
- *Return: Always 0
- */
-int main(
-    int argc __attribute__((unused)),
-    char *argv[])
-{
-    int line_number;
-    char *method_file, *content;
-    FILE *fd;
-    size_t len;
-
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    method_file = argv[1];
-    fd = fopen(method_file, "r");
-    if (fd == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", method_file);
-        exit(EXIT_FAILURE);
-    }
-
-    line_number = 0;
-    content = NULL;
-    while (getline(&content, &len, fd) != EOF)
-    {
-        line_number++;
-        parse_method(content, line_number);
-    }
-    return (0);
+    free(globals.lineptr);
+    free_stack(stack);
+    fclose(globals.fp);
+    return (EXIT_SUCCESS);
 }
--- monty.h
+++ monty.h
@@ -1,16 +1,24 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdlib.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <string.h>
-#include <stdlib.h>
+/**
+ * struct globals_s - struct contains global variables.
+ * @data: value to initialize nodes.
+ * @fp: a file pointer.
+ * @lineptr: pointer to a dynamic buffer.
+ * @token: a pointer to the next token.
+ */
+typedef struct globals_s
+{
+    int data;
+    FILE *fp;
+    char *lineptr, *token;
+} globals_t;
 
-#define EMPTY_STR ""
+extern globals_t globals;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -19,13 +27,13 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
 
 /**
@@ -34,43 +42,44 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-/** PROTOTYPES **/
 
-/** stack_actions.c **/
+/* opfunct1 */
 void push(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-
 void pall(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *stack);
 void pint(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 
+/* opfunct2 */
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
+
+/* opfunct3 */
 void mul(stack_t **stack, unsigned int line_number);
-void _div(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
+
+/* opfunct4 */
 void rotr(stack_t **stack, unsigned int line_number);
-void free_stack(stack_t *stack);
+void push_q(stack_t **stack, unsigned int line_number);
 
-void stack(stack_t **stack, unsigned int line_number);
-void queue(stack_t **stack, unsigned int line_number);
-void add_node(stack_t **stack, unsigned int line_number);
+/* monty_check */
+void error_handle(stack_t **stack, unsigned int line_number, int error_type);
+void check_op(stack_t **stack, unsigned int line_number);
+void parse_arg(int argc, char *argv[]);
+void read_line(stack_t **stack);
+void parse_num(stack_t **stack, unsigned int line_number);
 
-void (*get_method(char *method))(stack_t **stack, unsigned int line_number);
-
-extern char *global_stack;
-char *global_stack;
-extern stack_t *global_stack_head;
-stack_t *global_stack_head;
-#endif
+#endif /* MONTY_H */
